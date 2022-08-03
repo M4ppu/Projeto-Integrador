cdd42b275d71930060643a553f7eb627f69791e3
Luiza <luiza-97@hotmail.com>
2022-08-03 10:33:58 -0300

    Começo projeto integrador

---
+++ projetointegrador/src/main/java/org/generation/projetointegrador/repository/TemaRepository.java
@@ -0,0 +1,13 @@
+package org.generation.projetointegrador.repository;
+
+import java.util.List;
+
+import org.generation.projetointegrador.model.TemaModel;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.repository.query.Param;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface TemaRepository extends JpaRepository<TemaModel, Long>{
+	public List<TemaModel> findAllByDescricaoContainingIgnoreCase(@Param("descricao") String descricao);
+}
