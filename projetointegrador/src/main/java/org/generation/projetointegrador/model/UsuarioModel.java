9a6ad9ceb158698617db2cf4a71b7d8fda71d814
Luiza <luiza-97@hotmail.com>
2022-08-17 11:51:42 -0300

    código com bug

--- projetointegrador/src/main/java/org/generation/projetointegrador/model/UsuarioModel.java
+++ projetointegrador/src/main/java/org/generation/projetointegrador/model/UsuarioModel.java
@@ -16,6 +16,8 @@ import javax.validation.constraints.Size;
 
 import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
 
+import io.swagger.v3.oas.annotations.media.Schema;
+
 @Entity
 @Table (name = "usuario")
 public class UsuarioModel {
@@ -29,6 +31,7 @@ public class UsuarioModel {
 	@Size (min = 3, max = 100)
 	private String nome;
 	
+	@Schema(example = "email@email.com.br")
 	@NotNull(message = "O Atributo Usuário é Obrigatório!")
 	@Email(message = "O Atributo Usuário deve ser um email válido!")
 	private String usuario;
